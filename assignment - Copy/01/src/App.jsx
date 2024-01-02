543495037851f0b2baae84a561f729cace96b1b9
nehaltariq357 <143392985+nehaltariq357@users.noreply.github.com>
2024-01-02 05:06:06 -0800

    Add files via upload

---
+++ assignment - Copy/01/src/App.jsx
@@ -0,0 +1,246 @@
+import React from 'react'
+import Nav from './Components/Nav'
+
+
+function App() {
+  return (
+  <>  
+    <div className=' m-0 h-12 '>
+    <ul className='flex gap-1 justify-evenly p-3'><i><img className='ml-40 pb-5	 mr-14	' src="https://template.hasthemes.com/daxone/daxone/assets/images/logo/logo-1.png" alt="logo" /></i>
+      <li>home</li>
+      <li>shope</li>
+      <li>mens</li>
+      <li>page</li>
+      <li>blog</li>
+      <li>contact</li>
+      <i><img className='ml-40 mr-14' src="Images/Cart.jpg" alt="" /></i>
+    </ul>
+</div>
+
+
+<div className='flex'>
+  <div className='	 w-3/6	'><img src="https://template.hasthemes.com/daxone/daxone/assets/images/slider/hm1-single-1.png" width='500' alt="" /></div>
+
+
+  <div className='h-auto	 w-3/6 pt-44	'>
+    <h1 className='font-bold'>comfort chair</h1>
+    <p className=''>collection from daxon & get 30% discount</p>
+    <button type="button" className="btn btn-outline-danger">shop now</button>
+
+  </div>
+
+</div>
+
+
+
+
+<div>
+
+ <div className=''>
+  <h1 className='ml-96'>best sell</h1>
+  <p className='w-96 ml-72'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quasi fuga repudiandae voluptatum omni.</p>
+ </div>
+
+</div>
+
+
+<div class="container">
+  <div class="row row-cols-4">
+    <div class="col"><div class="card">
+  <img  src="https://template.hasthemes.com/daxone/daxone/assets/images/product/pro-hm1-1.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">chair</h5>
+    <p class="card-text">golden easy spot chair</p>
+    <p>$210.00</p>
+
+  </div>
+</div></div>
+    <div class="col"><div class="card">
+  <img  src="https://template.hasthemes.com/daxone/daxone/assets/images/product/pro-hm1-2.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">chair</h5>
+    <p class="card-text">golden easy spot chair</p>
+    <p>$210.00</p>
+    
+  </div>
+</div></div>
+    <div class="col"><div class="card">
+  <img  src="https://template.hasthemes.com/daxone/daxone/assets/images/product/pro-hm1-3.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">chair</h5>
+    <p class="card-text">golden easy spot chair</p>
+    <p>$210.00</p>
+   
+  </div>
+</div></div>
+    <div class="col"><div class="card">
+  <img  src="https://template.hasthemes.com/daxone/daxone/assets/images/product/pro-hm1-4.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">chair</h5>
+    <p class="card-text">golden easy spot chair</p>
+    <p>$210.00</p>
+    
+  </div>
+</div></div>
+  </div>
+</div>
+
+
+<div>
+
+<div className='flex ml-44 mt-16 '>
+  <div className='	 w-
+	'><img src="https://template.hasthemes.com/daxone/daxone/assets/images/banner/banner-1.jpg" width='500' alt="" /></div>
+  
+
+
+  <div className='h-auto	 w-3/6 mt-8 ml-14	'>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, tenetur!</p>
+    <h1 className='font-bold w-72'>winter discount upto 30%</h1>
+    <p className=''>winter discount upto 30% Lorem ipsum dolor sit amet.</p>
+    <button type="button" className="btn btn-outline-danger">shop now</button>
+
+  </div>
+
+</div>
+
+
+</div>
+
+<div>
+
+ <div className=''>
+  <h1 className='ml-96 mt-6 mb-6'>All Products</h1>
+  <p className='w-96 ml-72'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quasi fuga repudiandae voluptatum omni.</p>
+ </div>
+
+</div>
+
+<div class="container">
+  <div class="row row-cols-4">
+    <div class="col"><div class="card">
+  <img  src="https://template.hasthemes.com/daxone/daxone/assets/images/product/pro-hm1-1.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">chair</h5>
+    <p class="card-text">golden easy spot chair</p>
+    <p>$210.00</p>
+
+  </div>
+</div></div>
+    <div class="col"><div class="card">
+  <img  src="https://template.hasthemes.com/daxone/daxone/assets/images/product/pro-hm1-2.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">chair</h5>
+    <p class="card-text">golden easy spot chair</p>
+    <p>$210.00</p>
+    
+  </div>
+</div></div>
+    <div class="col"><div class="card">
+  <img  src="https://template.hasthemes.com/daxone/daxone/assets/images/product/pro-hm1-3.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">chair</h5>
+    <p class="card-text">golden easy spot chair</p>
+    <p>$210.00</p>
+   
+  </div>
+</div></div>
+    <div class="col"><div class="card">
+  <img  src="https://template.hasthemes.com/daxone/daxone/assets/images/product/pro-hm1-4.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">chair</h5>
+    <p class="card-text">golden easy spot chair</p>
+    <p>$210.00</p>
+    
+  </div>
+</div></div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row row-cols-4">
+    <div class="col"><div class="card">
+  <img  src="https://template.hasthemes.com/daxone/daxone/assets/images/product/pro-hm1-5.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">chair</h5>
+    <p class="card-text">golden easy spot chair</p>
+    <p>$210.00</p>
+
+  </div>
+</div></div>
+    <div class="col"><div class="card">
+  <img  src="https://template.hasthemes.com/daxone/daxone/assets/images/product/pro-hm1-6.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">chair</h5>
+    <p class="card-text">golden easy spot chair</p>
+    <p>$210.00</p>
+    
+  </div>
+</div></div>
+    <div class="col"><div class="card">
+  <img  src="https://template.hasthemes.com/daxone/daxone/assets/images/product/pro-hm1-7.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">chair</h5>
+    <p class="card-text">golden easy spot chair</p>
+    <p>$210.00</p>
+   
+  </div>
+</div></div>
+    <div class="col"><div class="card">
+  <img  src="https://template.hasthemes.com/daxone/daxone/assets/images/product/pro-hm1-8.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">chair</h5>
+    <p class="card-text">golden easy spot chair</p>
+    <p>$210.00</p>
+    
+  </div>
+</div></div>
+  </div>
+</div>
+
+<div>
+
+<div>
+<div className="container h-56 ml-32 mt-16">
+  <div className="row row-cols-4 w-full">
+    <div className="col">UseFul Links
+    <ul>
+    <li>shping cat</li>
+    <li>wishlist</li>
+    <li>checkout</li>
+    <li>support</li>
+    </ul>
+    </div>
+
+    <div className="col">About 
+    <ul>
+    <li>shping cat</li>
+    <li>wishlist</li>
+    <li>checkout</li>
+    <li>support</li>
+    </ul>
+    </div>
+
+    <div className="col">News Lettter
+    <ul>
+    <li>shping cat</li>
+    <li>wishlist</li>
+    <li>checkout</li>
+    <li>support</li>
+    </ul>
+    </div>
+
+  </div>
+</div>
+</div>
+
+</div>
+
+
+
+  <Nav/>
+  </>
+  )
+}
+
+export default App
---
+++ assignment - Copy/01/src/Components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Nav() {
+  return 
+  <>
+  <div className='bg-yellow-500'>fgfdgfg
+
+  </div>
+  </>
+    
+  
+}
+
+export default Nav
